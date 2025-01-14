bd1e1e2d7d9a4938f8dfe3c42795568c9ceacc4c
Ben Mayer <info@leocodes.net>
2025-01-14 22:38:26 +0100

    Update

--- al-khaser/Al-khaser.cpp
+++ al-khaser/Al-khaser.cpp
@@ -185,7 +185,8 @@ int main(int argc, char* argv[])
 		exec_check(&disk_size_getdiskfreespace, TEXT("Checking disk size using GetDiskFreeSpaceEx "));
 		exec_check(&cpuid_is_hypervisor, TEXT("Checking if CPU hypervisor field is set using cpuid(0x1)"));
 		exec_check(&cpuid_hypervisor_vendor, TEXT("Checking hypervisor vendor using cpuid(0x40000000)"));
-		exec_check(&accelerated_sleep, TEXT("Check if time has been accelerated "));
+		// Causes hangups, sometimes
+		// exec_check(&accelerated_sleep, TEXT("Check if time has been accelerated "));
 		exec_check(&VMDriverServices, TEXT("VM Driver Services  "));
 		exec_check(&serial_number_bios_wmi, TEXT("Checking SerialNumber from BIOS using WMI "));
 		exec_check(&model_computer_system_wmi, TEXT("Checking Model from ComputerSystem using WMI "));
